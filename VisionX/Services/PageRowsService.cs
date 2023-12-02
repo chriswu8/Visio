366af84976534781efc572abbbaa499945abdf31
Chris Wu <zezemint@gmail.com>
2023-12-02 09:43:06 -0800

    guarded against conflict scheduling

--- VisionX/Services/PageRowsService.cs
+++ VisionX/Services/PageRowsService.cs
@@ -9,12 +9,16 @@ namespace VisionX.Services
     public interface IPageRowsService
     {
         bool RowClicked { get; set; }
+        bool SelectedRowEmpty { get; set; }
+
         event EventHandler RowClickedChanged;
     }
 
     public class PageRowsService : IPageRowsService
     {
         private bool _rowClicked;
+        private bool _selectedRowEmpty;
+
 
         public bool RowClicked
         {
@@ -26,11 +30,42 @@ namespace VisionX.Services
             }
         }
 
+        // public bool SelectedRowEmpty
+        // {
+        //     get => _selectedRowEmpty;
+        //     set => _selectedRowEmpty = value;
+        // }
+
+
         public event EventHandler RowClickedChanged;
 
         protected virtual void OnRowClickedChanged()
         {
             RowClickedChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        // ========================================
+
+        public bool SelectedRowEmpty
+        {
+            get => _selectedRowEmpty;
+            set
+            {
+                _selectedRowEmpty = value;
+                OnSelectedRowEmptyChanged(); // Invoke event when the value changes
+            }
+        }
+
+        public event EventHandler SelectedRowEmptyChanged;
+
+        protected virtual void OnSelectedRowEmptyChanged()
+        {
+            SelectedRowEmptyChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SetSelectedRowEmpty(bool value)
+        {
+            SelectedRowEmpty = value; // Set the SelectedRowEmpty property
         }
     }
 }
